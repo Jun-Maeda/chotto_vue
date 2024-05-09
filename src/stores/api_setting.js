9c56a7d906f14a029637bfaef60823e3fe376f83
maedajun <jun.mermaid.voice@gmail.com>
2024-05-09 23:57:03 +0900

    add:最低限の修正

--- src/stores/api_setting.js
+++ src/stores/api_setting.js
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia'
 export const apiSettingStore = defineStore(
   'api_setting',
   () => {
-    const api_url = ref("https://misato.backend.hotel-chotto.com")
+    const api_url = ref("https://misato-backend.hotel-chotto.com")
     const api_images = ref()
 
     // function resetEvent() {
